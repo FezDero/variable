483329d80e9e77eb6b9c736f531d2b81f56c8b3c
FezDero <fezdero@gmail.com>
2024-11-28 21:46:53 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+Completed_Home_Works = 12
+Time_spent = 1.5
+Name_courses = 'Python'
+Time_for_one_task = Time_spent / Completed_Home_Works
+print (Name_courses, "всего задач:", Completed_Home_Works, "затрачено часов:", Time_spent, 'среднее время выполнения:', Time_for_one_task)
